c0ac77fb1f80544e566f24fbc59f5a18f185c890
Nicolás Morales <32245248+nichromatic@users.noreply.github.com>
2017-11-26 23:32:45 +0100

    Recuperación de cambios perdidos por la versión 3

--- Proyecto/src/main/resources/static/js/prefabs/Hero.js
+++ Proyecto/src/main/resources/static/js/prefabs/Hero.js
@@ -3,9 +3,33 @@ var ProceduralGeneration = ProceduralGeneration || {};
 ProceduralGeneration.Hero = function (game_state, name, position, properties) {
     ProceduralGeneration.Prefab.call(this, game_state, name, position, properties);
     
-    this.anchor.setTo(0.5);
+    var tile_size = 64; // Tamaño del tile
+    var offset_x = 30; // Offset X del sprite dado su anchor
+    var offset_y = 15; // Offset Y del sprite dado su anchor
+
+        // Coloca al jugador en el sitio correcto y mirando al lado correspondiente a la ultima puerta por la que entró
+    if (last_door == "S") {
+        this.x = (6 * tile_size) - offset_x;
+        this.y = (3 * tile_size) - offset_y;
+    } else if (last_door == "N") {
+        this.x = (6 * tile_size) - offset_x;
+        this.y = (8 * tile_size) + offset_y*4;
+    } else if (last_door == "W") {
+        this.x = (8 * tile_size) + offset_x;
+        this.y = (6 * tile_size) - offset_y + 5;
+    } else if (last_door == "E") {
+        this.x = (3 * tile_size) - offset_x;
+        this.y = (6 * tile_size) - offset_y + 10;
+    }
+
+    // El anchor del sprite es necesario que esté a los pies del personaje, para que no se "suba por encima" de los obstáculos al chocarse desde arriba
+    this.anchor.setTo(0.5, 1); // x = mitad del sprite, y = abajo del todo del sprite
     
     this.walking_speed = +properties.walking_speed;
+
+    // Cambiamos el tamaño del body (físico, no del sprite) para que las colisiones sean mas ajustadas
+    this.body.width = 25;
+    this.body.height = 25;
 
     this.game_state.game.physics.arcade.enable(this);
     this.body.collideWorldBounds = true;
