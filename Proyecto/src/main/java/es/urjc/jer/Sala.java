e13c5ceab3b9e7d2094b6b779e25ab5766430a0e
Víctor Manuel Palma Morales <32171139+vmpalmamorales@users.noreply.github.com>
2017-11-26 16:54:52 +0100

    Implementacion API REST en el servidor para la gestión de salas
    Peticiones GET, POST y DELETE asi como los modelos de datos necesarios y la logica de negocio necesaria.

--- Proyecto/src/main/java/es/urjc/jer/Sala.java
+++ Proyecto/src/main/java/es/urjc/jer/Sala.java
@@ -4,9 +4,46 @@ import java.util.ArrayList;
 
 public class Sala {
 	
-	private ArrayList<Double> rnform;
+	private ArrayList<Double> rnform; //"forma" de la sala
+	private ArrayList<Integer> rntile; //numero de tiles en cada sala
+	private ArrayList<Point> rnsize; //tamaño del tile 
+	private ArrayList<Integer> rntilecenter; //posicion del tile en sala
+	private ArrayList<Integer> rnprefab; //numero de prefabs por sala
 	
 	public Sala() {}
+	
+	
+	public ArrayList<Integer> getRntile() {
+		return rntile;
+	}
+
+	public void setRntile(ArrayList<Integer> rntile) {
+		this.rntile = rntile;
+	}
+
+	public ArrayList<Point> getRnsize() {
+		return rnsize;
+	}
+
+	public void setRnsize(ArrayList<Point> rnsize) {
+		this.rnsize = rnsize;
+	}
+
+	public ArrayList<Integer> getRntilecenter() {
+		return rntilecenter;
+	}
+
+	public void setRntilecenter(ArrayList<Integer> rntilecenter) {
+		this.rntilecenter = rntilecenter;
+	}
+
+	public ArrayList<Integer> getRnprefab() {
+		return rnprefab;
+	}
+
+	public void setRnprefab(ArrayList<Integer> rnprefab) {
+		this.rnprefab = rnprefab;
+	}
 
 	public ArrayList<Double> getRnform() {
 		return rnform;
--- Proyecto/src/main/java/es/urjc/jer/SalaController.java
+++ Proyecto/src/main/java/es/urjc/jer/SalaController.java
@@ -1,9 +1,12 @@
 package es.urjc.jer;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SalaController {
 	
 	private ArrayList<Sala> lista_salas = new ArrayList<>();
+	private ArrayList<Point> lista_size = initSize();
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Sala> getSala(@PathVariable int id) {
@@ -32,19 +36,107 @@ public class SalaController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Sala nuevaSala(@RequestBody Sala sala) {
-		/*
-		ArrayList<Double> rnform = new ArrayList<>();
+
+		sala.setRnform(randomform());
+		sala.setRntile(randomtile());
+		sala.setRnsize(randomsize());
+		sala.setRntilecenter(randomtilecenter());	
+		sala.setRnprefab(randomprefab());
 		
-		for (int i = 0; i < 10; i++){
-	        rnform.add(Math.random()*10);
-	    }
-		*/
 		lista_salas.add(sala);
 		
 		return sala;
 	}
 	
+	@GetMapping
+	public Collection<Sala> salas() {
+		return lista_salas;
+	}
 	
+	private ArrayList<Double> randomform (){
+		
+		ArrayList<Double> a = new ArrayList<>();
+		
+		for (int i = 0; i < 10; i++){
+	        a.add(Math.random());
+	    }
+		
+		return a;
+	}
 	
+	private ArrayList<Integer> randomtile (){
+		
+		Random rnd = new Random();
+		ArrayList<Integer> a = new ArrayList<>();
+		
+		for (int i = 0; i < 10; i++){
+	        a.add(rnd.nextInt(7) + 5);
+	    }
+		
+		return a;
+	}
+	
+	private ArrayList<Point> randomsize(){
+		
+		ArrayList<Point> a = new ArrayList<>();
+		Random rnd = new Random();
+		int pos;
+
+		for (int i = 0; i < 120; i++){
+			pos = rnd.nextInt(4);
+			a.add(lista_size.get(pos));
+	    }
+		
+		return a;
+	} 
+	
+	private ArrayList<Point> initSize () {
+		
+		ArrayList<Point> a = new ArrayList<>();
+		a.add(new Point(1,1));
+		a.add(new Point(1,2));
+		a.add(new Point(1,3));
+		a.add(new Point(2,1));
+		a.add(new Point(3,1));
+		
+		return a;
+	}
+	
+	private ArrayList<Integer> randomtilecenter(){
+		
+		ArrayList<Integer> a = new ArrayList<>();
+		Random rnd = new Random();
+		
+		for (int i = 0; i < 50; i++) {
+			a.add(rnd.nextInt(6)+3);
+			a.add(rnd.nextInt(6)+3);
+		}
+
+		return a;		
+	}
+	
+	private ArrayList<Integer> randomprefab(){
+		
+		ArrayList<Integer> a = new ArrayList<>();
+		
+		for (int i = 0; i < 41; i++) {
+			a.add((int) Math.round(Math.random()));
+		}
+
+		return a;		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Sala> borrarSala(@PathVariable int id) {
+
+		Sala savedSala = lista_salas.get(id);
+
+		if (savedSala != null) {
+			lista_salas.remove(id);
+			return new ResponseEntity<>(savedSala, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 	
 }
