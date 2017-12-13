477f5c3b38f1e6db7dc51a5635e2281c9fc958f5
Víctor Manuel Palma Morales <32171139+vmpalmamorales@users.noreply.github.com>
2017-12-13 01:15:18 +0100

    Comunicación via WS para unirse a salas

--- Proyecto/src/main/java/es/urjc/jer/PlayerHandler.java
+++ Proyecto/src/main/java/es/urjc/jer/PlayerHandler.java
@@ -27,6 +27,7 @@ public class PlayerHandler extends TextWebSocketHandler {
 		Player p = new Player();
 		p.setPlay(false);
 		players.put(session.getId(), p);
+		
 	}
 	
 	@Override
@@ -44,14 +45,29 @@ public class PlayerHandler extends TextWebSocketHandler {
 		
 		switch (node.get("protocolo").asText()) {
 		case "createSala_msg": 
-
 			Sala s = new Sala (salas.size(), 0, 0);
+			System.out.println(salas.size());
 			players.get(session.getId()).setPlay(true);
+			players.get(session.getId()).setIdsala(s.getId());
+			System.out.println("" + players.get(session.getId()).getIdsala());
 			s.addPlayer(players.get(session.getId()));
+			System.out.println("TAMAÑO ANTES" + salas.size());
 			salas.put(salas.size(), s);
-			notifySalaCreated(players, session, node, s.getNumJugadores());
+			System.out.println("TAMAÑO AHORA" + salas.size());
+
+			
+			notifySalaCreated(session, node, s.getNumJugadores());
+			break;
+			
+		case "joinSala_msg": 
+			
+			players.get(session.getId()).setPlay(true);
+			players.get(session.getId()).setIdsala(node.get("id").asInt());
+			salas.get(node.get("id").asInt()).addPlayer(players.get(session.getId()));
+			notifyJ2(node, session);
 			
 			break;
+			
 		case "player_msg":
 			//METODO PARA JUGADORES
 			break;
@@ -63,9 +79,22 @@ public class PlayerHandler extends TextWebSocketHandler {
 		}
 			
 	}
+	
+	private void notifyJ2 (JsonNode node, WebSocketSession session) throws IOException {
+		
+		ObjectNode join = mapper.createObjectNode();
+		join.put("protocolo", "joinSala_msg");
+		join.put("salaId", players.get(session.getId()).getIdsala());
+		for(WebSocketSession participant : sessions.values()) {
+			if(!participant.getId().equals(session.getId()) && (players.get(participant.getId()).getIdsala() == node.get("id").asInt())) {
+				participant.sendMessage(new TextMessage(join.toString()));
+				System.out.println("JOIN SALA: Message sent: " + join.toString());
+			}
+		}
+	}
 
 	//notifica a todos los jugadores que no esten en una partida
-	private void notifySalaCreated(Map<String, Player> players, WebSocketSession session, JsonNode node, int size) throws IOException {
+	private void notifySalaCreated(WebSocketSession session, JsonNode node, int size) throws IOException {
 		
 		ObjectNode newNode = mapper.createObjectNode();
 		newNode.put("protocolo", "createSala_msg");
@@ -78,6 +107,7 @@ public class PlayerHandler extends TextWebSocketHandler {
 				participant.sendMessage(new TextMessage(newNode.toString()));
 				System.out.println("CREATE SALA: Message sent: " + newNode.toString());
 			}
+			
 		}
 		
 	}
